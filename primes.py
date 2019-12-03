729a1961db0087f03438a9e2ea1286f3dc112d1b
Angle <45601608+3rw4t@users.noreply.github.com>
2019-12-03 08:28:25 -0300

    Update primes.py
    Mejora del algoritmo, ahora los numeros primos se guardan en el vector "primes[]"

--- primes.py
+++ primes.py
@@ -1,18 +1,33 @@
-nums = []
-tope = int(inputbox("Ingrese el tope: "))
-for numero in range(tope+1):
+import math
+primes = []
+tope = int(inputbox("Ingrese el limite: "))
+for a in range((tope % 2) + (math.trunc(tope / 2))):
+    acumulador = 0
     cont = 1
-    for value in range(numero -2):
-        if numero % (value + 2) == 0:
-            cont = 0
-            break
-        if (value + 2) >= numero/(value + 1):
-            cont = 1
-            break
-    if cont !=0 :
-        nums.append(numero)
-for a in range(len(nums) - 2):
-    print(a + 1 , ". " , nums[a + 2])
+    if a == 0:
+        numero = 2
+    else:
+        numero = 2 + ((a * 2) - 1)
+    a_str = str(numero)
+    if a_str[len(a_str) - 1] == "5" and numero != 5:
+        cont = 0
+    else:
+        cont = 1
+        for value in range(len(primes) - 1):
+            if numero % primes[value + 1] == 0:
+                cont = 0
+                break
+            if primes[value + 1] >= numero / primes[value]:
+                cont = 1
+                break
+    if cont != 0:
+        primes.append(numero)
+for a in range(len(primes)):
+    print(a + 1 , ". " , primes[a])
 #Tarda ~1,5s para 20.000
-#Tarda ~53s para 1.000.000
-#Tarda ~1140 s para 10.000.000
+#tarda ~6s para 100.000
+#Tarda ~20s para 1.000.000
+#Tarda ~264 s para 10.000.000
+#Tarda ~22426 s para 100.000.000
+
+#datos basados en la velocidad de mi pc
