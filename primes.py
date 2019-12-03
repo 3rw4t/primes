dc56e6713eb3cdd4193b2242aad0b0681a1eedd7
Angle <45601608+3rw4t@users.noreply.github.com>
2019-12-03 08:19:54 -0300

    Update primes.py
    Mejora del algoritmo, agregada la función de que el usuario elija el limite

--- primes.py
+++ primes.py
@@ -1,13 +1,18 @@
 nums = []
-tope= 23
+tope = int(inputbox("Ingrese el tope: "))
 for numero in range(tope+1):
     cont = 1
-    for value in range(numero - 2):
+    for value in range(numero -2):
         if numero % (value + 2) == 0:
             cont = 0
             break
-    if cont != 0:
+        if (value + 2) >= numero/(value + 1):
+            cont = 1
+            break
+    if cont !=0 :
         nums.append(numero)
 for a in range(len(nums) - 2):
     print(a + 1 , ". " , nums[a + 2])
-#Tarda ~7s para 20.000
+#Tarda ~1,5s para 20.000
+#Tarda ~53s para 1.000.000
+#Tarda ~1140 s para 10.000.000
